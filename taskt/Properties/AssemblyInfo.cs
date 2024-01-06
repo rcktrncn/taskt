f7e4fd52a997dacda04b7398b67dca9b05f9b9f9
rcktrncn <rcktrncn@hotmail.com>
2024-01-06 21:59:11 +0900

    v3.5.1.73

--- taskt/Properties/AssemblyInfo.cs
+++ taskt/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("taskt uob")]
-[assembly: AssemblyCopyright("Copyright ©2023")]
+[assembly: AssemblyCopyright("Copyright ©2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -45,6 +45,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.0.*")]
-[assembly: AssemblyFileVersion("3.5.1.72")]
+[assembly: AssemblyFileVersion("3.5.1.73")]
 //[assembly: AssemblyVersion("0.0.0.2")]
 //[assembly: AssemblyFileVersion("0.0.0.2")]
