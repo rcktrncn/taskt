f83fb45ada503be7a5ee8abe8010af37d93316ef
rcktrncn <rcktrncn@hotmail.com>
2024-03-31 20:46:51 +0900

    add interface, class

---
+++ taskt/Core/Automation/Commands/Dictionary/IDictionaryGetFromDictionaryProperties.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    public interface IDictionaryGetFromDictionaryProperties : ILDictionaryProperties
+    {
+        /// <summary>
+        /// Variable name to store the result obtained from Dictionary
+        /// </summary>
+        string v_Result { get; set; }
+    }
+}
