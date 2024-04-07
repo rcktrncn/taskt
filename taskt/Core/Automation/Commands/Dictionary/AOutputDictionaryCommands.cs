8a10c64b91ed6c740aabc27262b86d59c86a6e97
rcktrncn <rcktrncn@hotmail.com>
2024-04-07 14:28:43 +0900

    add new class

---
+++ taskt/Core/Automation/Commands/Dictionary/AOutputDictionaryCommands.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for Both Dictionary commands
+    /// </summary>
+    public abstract class AOutputDictionaryCommands : AInputDictionaryCommands
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(DictionaryControls), nameof(DictionaryControls.v_OutputDictionaryName))]
+        [PropertyParameterOrder(5000)]
+        public override string v_Dictionary { get; set; }
+    }
+}
