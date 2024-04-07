0353973a9d71afd45acfe6dae209d2fda6156dd3
rcktrncn <rcktrncn@hotmail.com>
2024-04-07 15:12:04 +0900

    add new interface, classes

---
+++ taskt/Core/Automation/Commands/Dictionary/ILDictionaryCreateFromDictionary.cs
@@ -0,0 +1,18 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// Create Dictionary from Dictionary properties
+    /// </summary>
+    public interface ILDictionaryCreateFromDictionary: ILExpandableProperties, ICanHandleDictionary
+    {
+        /// <summary>
+        /// Dictionary variable name to Create New Dictionary
+        /// </summary>
+        string v_TargetDictionary { get; set; }
+
+        /// <summary>
+        /// Dictionary variable name to Store New Dictionary
+        /// </summary>
+        string v_NewDictionary { get; set; }
+    }
+}
