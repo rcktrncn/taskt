cb1ef1ea95ec3420c5abdb802e15c1b6e1ac4921
rcktrncn <rcktrncn@hotmail.com>
2024-04-13 16:46:39 +0900

    add class

---
+++ taskt/Core/Automation/Commands/Dictionary/ADictionaryKeyActionCommands.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    public abstract class ADictionaryKeyActionCommands : ADictionaryKeyCommands, IDictionaryKeyActionProperties
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(DictionaryControls), nameof(DictionaryControls.v_WhenKeyDoesNotExists))]
+        public string v_WhenKeyDoesNotExists { get; set; }
+    }
+}
