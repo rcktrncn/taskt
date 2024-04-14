bb09aa23dc9463c1eee6513254eee8783b48b038
rcktrncn <rcktrncn@hotmail.com>
2024-04-14 21:49:41 +0900

    add abstract class

---
+++ taskt/Core/Automation/Commands/DataTable/AInputDataTableCommands.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    public class AInputDataTableCommands : ScriptCommand, ILDataTableProperties
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(DataTableControls), nameof(DataTableControls.v_InputDataTableName))]
+        public string v_DataTable { get; set; }
+    }
+}
