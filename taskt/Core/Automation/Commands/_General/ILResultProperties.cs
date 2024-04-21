6408e2890f467ca47e9df996f0ff5cb8dcbe4ce3
rcktrncn <rcktrncn@hotmail.com>
2024-04-21 20:39:01 +0900

    add IResult

---
+++ taskt/Core/Automation/Commands/_General/ILResultProperties.cs
@@ -0,0 +1,13 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// Result properties
+    /// </summary>
+    public interface ILResultProperties
+    {
+        /// <summary>
+        /// Variable Name to Store Result
+        /// </summary>
+        string v_Result { get; set; }
+    }
+}
