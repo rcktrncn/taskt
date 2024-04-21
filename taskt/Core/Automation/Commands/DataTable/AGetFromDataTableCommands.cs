7394e22f9c6fdac27f4da62915cd408b962f1998
rcktrncn <rcktrncn@hotmail.com>
2024-04-21 19:34:21 +0900

    add interface, abstract class

---
+++ taskt/Core/Automation/Commands/DataTable/AGetFromDataTableCommands.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    public abstract class AGetFromDataTableCommands : AInputDataTableCommands, IDataTableGetFromDataTable
+    {
+        [XmlAttribute]
+        [PropertyParameterOrder(10000)]
+        public abstract string v_Result { get; set; }
+    }
+}
---
+++ taskt/Core/Automation/Commands/DataTable/IDataTableGetFromDataTable.cs
@@ -0,0 +1,13 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// Get something from DataTable properties
+    /// </summary>
+    public interface IDataTableGetFromDataTable : ILDataTableProperties
+    {
+        /// <summary>
+        /// Variable Name to Store Result
+        /// </summary>
+        string v_Result { get; set; }
+    }
+}
