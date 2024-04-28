f22ce9788c2e4e47a20f443c500a0b0310087cba
rcktrncn <rcktrncn@hotmail.com>
2024-04-28 16:25:48 +0900

    add new interface, extension methods

---
+++ taskt/Core/Automation/Commands/List/EM_ListResultPropertiesExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace taskt.Core.Automation.Commands
+{
+    public static class EM_ListResultPropertiesExtensionMethods
+    {
+        /// <summary>
+        /// Store List in User Variable
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="list"></param>
+        /// <param name="engine"></param>
+        public static void StoreListInUserVariable(this IListResultProperties command, List<string> list, Engine.AutomationEngineInstance engine)
+        {
+            command.StoreListInUserVariable(list, nameof(command.v_Result), engine);
+        }
+    }
+}
