c84474ac59e0bd9a0ab373fbca19b559df64dfa1
rcktrncn <rcktrncn@hotmail.com>
2024-04-28 11:05:17 +0900

    add new interface, abstract class

---
+++ taskt/Core/Automation/Commands/DataTable/ADataTableRowCommands.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for DataTable Row commands
+    /// </summary>
+    public abstract class ADataTableRowCommands : ADataTableInputDataTableCommands, IDataTableRowProperties
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(DataTableControls), nameof(DataTableControls.v_RowIndex))]
+        [PropertyParameterOrder(6000)]
+        public virtual string v_RowIndex { get; set; }
+    }
+}
---
+++ taskt/Core/Automation/Commands/DataTable/IDataTableRowProperties.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// DataTable Row Properties
+    /// </summary>
+    public interface IDataTableRowProperties : ILDataTableProperties, ILDataTableRowPositionProperties
+    {
+        // no imple
+    }
+}
