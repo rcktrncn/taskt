4b7cc0ab95df8c6fb522c6bdf0c0a6713b55e39b
rcktrncn <rcktrncn@hotmail.com>
2024-04-29 16:59:40 +0900

    add new abstract class, extension methods

---
+++ taskt/Core/Automation/Commands/DataTable/EM_DataTableExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace taskt.Core.Automation.Commands
+{
+    public static class EM_DataTableExtensionMethods
+    {
+        /// <summary>
+        /// Get DataTable Column Name List
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        public static List<string> GetColumnNameList(this DataTable table)
+        {
+            return table.Columns.Cast<DataColumn>().Select(x => x.ColumnName).ToList();
+        }
+    }
+}
