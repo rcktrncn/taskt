76f5f64e4bfc7d8bcceecd1740f63f0888abd285
rcktrncn <rcktrncn@hotmail.com>
2024-05-05 11:24:40 +0900

    add new interface, classes

---
+++ taskt/Core/Automation/Commands/DataTable/ADataTableColumnRowCommands.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+using taskt.Core.Automation.Commands.DataTable;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for DataTable Column Row specified commands
+    /// </summary>
+    public abstract class ADataTableColumnRowCommands : ADataTableInputDataTableCommands, IDataTableColumnRowPositionProperties
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(DataTableControls), nameof(DataTableControls.v_ColumnType))]
+        [PropertyParameterOrder(7000)]
+        public string v_ColumnType { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(DataTableControls), nameof(DataTableControls.v_ColumnNameIndex))]
+        [PropertyParameterOrder(8000)]
+        public string v_ColumnIndex { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(DataTableControls), nameof(DataTableControls.v_RowIndex))]
+        [PropertyParameterOrder(9000)]
+        public string v_RowIndex { get; set; }
+    }
+}
---
+++ taskt/Core/Automation/Commands/DataTable/IDataTableColumnRowPositionProperties.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// DataTable Column Row specified properties
+    /// </summary>
+    public interface IDataTableColumnRowPositionProperties : ILDataTableProperties, ILDataTableColumnPositionProperties, ILDataTableRowPositionProperties
+    {
+        // only imple
+    }
+}
