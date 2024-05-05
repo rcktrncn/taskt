61cff0782647e31a2f8a46f937ce5e6f96cfea28
rcktrncn <rcktrncn@hotmail.com>
2024-05-05 13:20:52 +0900

    add new extension method, but not work now

---
+++ taskt/Core/Automation/Commands/_General/EM_HaveDataTablePropertiesExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+
+namespace taskt.Core.Automation.Commands._General
+{
+    public static class EM_HaveDataTablePropertiesExtensionMethods
+    {
+        // TODO: make it!
+        //public static DataTable GetDataTableFromParameterName(this IHaveDataTableElements command, string parameterName)
+        //{
+        //    var prop = command.GetProperty(parameterName);
+        //    var value = prop?.GetValue(command) ?? null;
+        //    if (value is DataTable tbl)
+        //    {
+        //        return tbl;
+        //    }
+        //    else
+        //    {
+        //        throw new Exception($"Parameter '{parameterName}' is not DataTable");
+        //    }
+        //}
+    }
+}
