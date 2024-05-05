4823b258e97ff3d54f1f1e570bcda090339c4461
rcktrncn <rcktrncn@hotmail.com>
2024-05-05 13:16:03 +0900

    add summery

--- taskt/Core/Automation/Commands/_General/IHaveDataTableElements.cs
+++ taskt/Core/Automation/Commands/_General/IHaveDataTableElements.cs
@@ -5,6 +5,9 @@
     /// </summary>
     public interface IHaveDataTableElements
     {
+        /// <summary>
+        /// Process before Validate ScriptCommand parameters
+        /// </summary>
         void BeforeValidate();
     }
 }
