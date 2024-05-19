2feae61eb54d4730a931f57c6ba5e5b0e81918a4
rcktrncn <rcktrncn@hotmail.com>
2024-05-19 15:27:03 +0900

    support loop times variable

--- taskt/Core/Automation/Commands/Loop/BeginContinousLoopCommand.cs
+++ taskt/Core/Automation/Commands/Loop/BeginContinousLoopCommand.cs
@@ -1,43 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
-using taskt.UI.CustomControls;
-using taskt.UI.Forms;
+using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
 
 namespace taskt.Core.Automation.Commands
 {
 
     [Serializable]
     [Attributes.ClassAttributes.Group("Loop")]
+    [Attributes.ClassAttributes.CommandSettings("Loop Continuously")]
     [Attributes.ClassAttributes.Description("This command allows you to repeat actions continuously.  Any 'Begin Loop' command must have a following 'End Loop' command.")]
     [Attributes.ClassAttributes.UsesDescription("Use this command when you want to perform a series of commands an endless amount of times.")]
     [Attributes.ClassAttributes.ImplementationDescription("This command recursively calls the underlying 'BeginLoop' Command to achieve automation.")]
     [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_startloop))]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
     public sealed class BeginContinousLoopCommand : ScriptCommand
     {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_Result))]
+        [PropertyDescription("Variable Name to Store Loop Times (First Time Value is 0)")]
+        [PropertyIsOptional(true)]
+        public string v_LoopTimes { get; set; }
 
         public BeginContinousLoopCommand()
         {
-            this.CommandName = "BeginContinousLoopCommand";
-            this.SelectionName = "Loop Continuously";
-            this.CommandEnabled = true;
-            this.CustomRendering = true;
+            //this.CommandName = "BeginContinousLoopCommand";
+            //this.SelectionName = "Loop Continuously";
+            //this.CommandEnabled = true;
+            //this.CustomRendering = true;
         }
 
-        public override void RunCommand(Engine.AutomationEngineInstance engine, Core.Script.ScriptAction parentCommand)
+        public override void RunCommand(Engine.AutomationEngineInstance engine, Script.ScriptAction parentCommand)
         {
-            Core.Automation.Commands.BeginContinousLoopCommand loopCommand = (Core.Automation.Commands.BeginContinousLoopCommand)parentCommand.ScriptCommand;
+            var loopCommand = (BeginContinousLoopCommand)parentCommand.ScriptCommand;
+
+            Action<decimal> loopTimesAction;
+            if (!string.IsNullOrEmpty(v_LoopTimes))
+            {
+                loopTimesAction = new Action<decimal>(num =>
+                {
+                    num.StoreInUserVariable(engine, v_LoopTimes);
+                });
+            }
+            else
+            {
+                loopTimesAction = new Action<decimal>(num =>
+                {
+                    // nothing
+                });
+            }
 
             engine.ReportProgress("Starting Continous Loop From Line " + loopCommand.LineNumber);
 
+            decimal count = 0;
             while (true)
             {
-
+                loopTimesAction(count);
 
                 foreach (var cmd in parentCommand.AdditionalScriptCommands)
                 {
                     if (engine.IsCancellationPending)
+                    {
                         return;
+                    }
 
                     engine.ExecuteCommand(cmd);
 
@@ -55,21 +80,31 @@ namespace taskt.Core.Automation.Commands
                         break;
                     }
                 }
+
+                try
+                {
+                    count++;
+                }
+                catch
+                {
+                    count = decimal.MinValue;
+                }
             }
         }
-        public override List<Control> Render(UI.Forms.ScriptBuilder.CommandEditor.frmCommandEditor editor)
-        {
-            base.Render(editor);
 
-            RenderedControls.Add(CommandControls.CreateDefaultLabelFor("v_Comment", this));
-            RenderedControls.Add(CommandControls.CreateDefaultInputFor("v_Comment", this, 100, 300));
+        //public override List<Control> Render(UI.Forms.ScriptBuilder.CommandEditor.frmCommandEditor editor)
+        //{
+        //    base.Render(editor);
 
-            return RenderedControls;
-        }
+        //    RenderedControls.Add(CommandControls.CreateDefaultLabelFor("v_Comment", this));
+        //    RenderedControls.Add(CommandControls.CreateDefaultInputFor("v_Comment", this, 100, 300));
 
-        public override string GetDisplayValue()
-        {
-            return base.GetDisplayValue();
-        }
+        //    return RenderedControls;
+        //}
+
+        //public override string GetDisplayValue()
+        //{
+        //    return base.GetDisplayValue();
+        //}
     }
 }
