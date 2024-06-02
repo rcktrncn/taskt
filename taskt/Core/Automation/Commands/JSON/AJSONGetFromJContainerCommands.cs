b4bdad502a1f241a40e3cf80111a25d614bd2b9b
rcktrncn <rcktrncn@hotmail.com>
2024-06-02 15:54:08 +0900

    add new abstract class

---
+++ taskt/Core/Automation/Commands/JSON/AJSONGetFromJContainerCommands.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for Get From JContainer(Object or Array) commands
+    /// </summary>
+    public abstract class AJSONGetFromJContainerCommands : AJSONGetFromJSONCommands, IJsonInputJContainer
+    {
+        // nothing
+    }
+}
