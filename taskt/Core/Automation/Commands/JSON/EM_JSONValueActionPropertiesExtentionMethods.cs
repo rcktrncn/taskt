9eb3abaf1c6cc2139f59d1cea3f371589b41de94
rcktrncn <rcktrncn@hotmail.com>
2024-06-09 21:59:00 +0900

    add new method

--- taskt/Core/Automation/Commands/JSON/EM_JSONValueActionPropertiesExtentionMethods.cs
+++ taskt/Core/Automation/Commands/JSON/EM_JSONValueActionPropertiesExtentionMethods.cs
@@ -45,5 +45,102 @@ namespace taskt.Core.Automation.Commands
                     throw new Exception($"Spceified Value is NOT JSON. Value: '{str}'");
             }
         }
+
+        /// <summary>
+        /// Expand Value or User Variable as JSON-Supported value in JSON Value
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="engine"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static JToken ExpandValueOrVariableValueAsJSONSupportedValueInJSONValue(this IJSONValueActionProperties command, Engine.AutomationEngineInstance engine)
+        {
+            string str;
+            JContainer json = null;
+            try
+            {
+                (str, json, _) = command.ExpandValueOrUserVariableAsJSON(nameof(command.v_Value), engine);
+            }
+            catch
+            {
+                str = ((ScriptCommand)command).ExpandValueOrUserVariable(nameof(command.v_Value), "Value", engine);
+            }
+
+            var t = ((ScriptCommand)command).ExpandValueOrUserVariableAsSelectionItem(nameof(command.v_ValueType), engine);
+
+            if (json is JArray ary)
+            {
+                switch (t)
+                {
+                    case "array":
+                    case "auto":
+                        return ary;
+
+                    default:
+                        throw new Exception($"Spceified Value is NOT JSON Array. Value: '{str}'");
+                }
+            }
+            else if (json is JObject obj)
+            {
+                switch (t)
+                {
+                    case "object":
+                    case "auto":
+                        return obj;
+
+                    default:
+                        throw new Exception($"Spceified Value is NOT JSON Object. Value: '{str}'");
+                }
+                
+            }
+            else if (decimal.TryParse(str, out decimal num))
+            {
+                switch (t)
+                {
+                    case "number":
+                    case "auto":
+                        return new JValue(num);
+
+                    default:
+                        throw new Exception($"Spceified Value is NOT Number. Value: '{str}'");
+                }
+            }
+            else if (bool.TryParse(str, out bool tf))
+            {
+                switch (t)
+                {
+                    case "boolean":
+                    case "auto":
+                        return new JValue(tf);
+
+                    default:
+                        throw new Exception($"Spceified Value is NOT Boolean. Value: '{str}'");
+                }
+            }
+            else if (str.ToLower() == "null")
+            {
+                switch (t)
+                {
+                    case "null":
+                    case "auto":
+                        return null;
+
+                    default:
+                        throw new Exception($"Spceified Value is NOT Null. Value: '{str}'");
+                }
+            }
+            else
+            {
+                switch (t)
+                {
+                    case "text":
+                    case "auto":
+                        return new JValue(str);
+
+                    default:
+                        throw new Exception($"Spceified Value is NOT Text. Value: '{str}'");
+                }
+            }
+        }
     }
 }
