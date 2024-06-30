aa9e76eeba3d476a4b35e833e01e69f410e4d74e
rcktrncn <rcktrncn@hotmail.com>
2024-06-30 11:33:55 +0900

    add new set commands

---
+++ taskt/Core/Automation/Commands/JSON/AJSONSetValueOfJContainerCommands.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// Set Value of JContainer commands
+    /// </summary>
+    public abstract class AJSONSetValueOfJContainerCommands : AJSONAddInsertSetJContainerCommands
+    {
+        [XmlAttribute]
+        [PropertyDescription("Value to Set")]
+        public override string v_Value { get; set; }
+    }
+}
