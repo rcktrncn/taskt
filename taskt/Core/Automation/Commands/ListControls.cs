609e6d3a3c027e652037817a14938f49611b3118
rcktrncn <rcktrncn@hotmail.com>
2024-08-03 11:29:22 +0900

    remove using

--- taskt/Core/Automation/Commands/ListControls.cs
+++ taskt/Core/Automation/Commands/ListControls.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using taskt.Core.Automation.Attributes.PropertyAttributes;
 
 namespace taskt.Core.Automation.Commands
