1832c9336cbea9c36bdaaf2fe67c57f271ea7ab9
rcktrncn <rcktrncn@hotmail.com>
2024-08-17 13:06:31 +0900

    remove using

--- taskt/UI/Forms/ScriptEngine/frmScriptEngine.cs
+++ taskt/UI/Forms/ScriptEngine/frmScriptEngine.cs
@@ -11,7 +11,6 @@
 //WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //See the License for the specific language governing permissions and
 //limitations under the License.
-using Org.BouncyCastle.Utilities.Bzip2;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
