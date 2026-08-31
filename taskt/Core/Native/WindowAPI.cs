76a7336d005b41811659fa481a633067696bf723
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-08-31 22:20:55 +0900

    add user32 apis

--- taskt/Core/Native/WindowAPI.cs
+++ taskt/Core/Native/WindowAPI.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
+using taskt.Core.Automation.Commands;
 
 namespace taskt.Core.Native.Windows
 {
@@ -19,6 +17,15 @@ namespace taskt.Core.Native.Windows
         private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
         /// <summary>
+        /// set window state async
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="nCmdShow"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+
+        /// <summary>
         /// set window is ForeGround
         /// </summary>
         /// <param name="hWnd"></param>
@@ -30,5 +37,159 @@ namespace taskt.Core.Native.Windows
         /// window state is normal
         /// </summary>
         private const int WINDOW_NORMAL = 1;
+
+        /// <summary>
+        ///  window maximize value
+        /// </summary>
+        private const int MAXIMIZE = 3;
+        /// <summary>
+        /// window minimize value
+        /// </summary>
+        private const int MINIMIZE = 6;
+        /// <summary>
+        /// window restore value
+        /// </summary>
+        private const int RESTORE = 9;
+
+        /// <summary>
+        /// for close window by whnd
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="Msg"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+
+        /// <summary>
+        /// close value
+        /// </summary>
+        private static readonly UInt32 WM_CLOSE = 0x0010;
+
+        /// <summary>
+        /// get destkop window handle
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("user32.dll", SetLastError = false)]
+        private static extern IntPtr GetDesktopWindow();
+
+        /// <summary>
+        /// check window handle exists
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        private static extern bool IsWindow(IntPtr hWnd);
+
+        /// <summary>
+        /// get window title length
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        private static extern int GetWindowTextLengthW(IntPtr hWnd);
+
+        /// <summary>
+        /// get window title
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="text"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int GetWindowTextW(IntPtr hWnd, StringBuilder text, int count);
+
+        /// <summary>
+        /// check window is minimized
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        private static extern bool IsIconic(IntPtr hWnd);
+
+        /// <summary>
+        /// check window is maximized
+        /// </summary>
+        /// <param name="hWhnd"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        private static extern bool IsZoomed(IntPtr hWhnd);
+
+        /// <summary>
+        /// get active window handle
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        /// <summary>
+        /// enum window delegate
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="lparam"></param>
+        /// <returns></returns>
+        public delegate bool EnumWindowsDelegate(IntPtr hWnd, IntPtr lparam);
+
+        /// <summary>
+        /// enum all windows
+        /// </summary>
+        /// <param name="lpEnumFunc"></param>
+        /// <param name="lparam"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern int EnumWindows(EnumWindowsDelegate lpEnumFunc, IntPtr lparam);
+
+        /// <summary>
+        /// check window is visible
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+
+        /// <summary>
+        /// get window rect
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="lpRect"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        /// <summary>
+        /// move or resize window
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="hWndInsertAfter"></param>
+        /// <param name="x"></param>
+        /// <param name="Y"></param>
+        /// <param name="cx"></param>
+        /// <param name="cy"></param>
+        /// <param name="wFlags"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, uint wFlags);
+
+        private struct WINDOWPLACEMENT
+        {
+            uint length;
+            uint flags;
+            public uint showCmd;
+            System.Drawing.Point ptMinPosition;
+            System.Drawing.Point ptMaxPosition;
+            RECT rcNormalPosition;
+            RECT rcDevice;
+        }
+
+        /// <summary>
+        /// get window state
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="lpwndpl"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
     }
 }
