8fe0d3035a8c66a1395b69af27d9557b5ca8540e
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-09-20 21:17:15 +0900

    use native, remove class

--- taskt/Core/Automation/Commands/UIAutomationGroup/EM_CanHandleUIElementExtentionMethods.cs
+++ taskt/Core/Automation/Commands/UIAutomationGroup/EM_CanHandleUIElementExtentionMethods.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Automation;
 using taskt.Core.Automation.Commands.WindowGroup;
+using taskt.Core.Native.Windows;
 
 namespace taskt.Core.Automation.Commands.UIAutomationGroup
 {
@@ -49,7 +50,8 @@ namespace taskt.Core.Automation.Commands.UIAutomationGroup
             var walker = TreeWalker.RawViewWalker;
             try
             {
-                var desktopHandle = EM_CanHandleDesktopWindowHandleExtensionMethods.GetDesktopWindowHandle();
+                //var desktopHandle = EM_CanHandleDesktopWindowHandleExtensionMethods.GetDesktopWindowHandle();
+                var desktopHandle = WindowAPI.GetDesktopWindowHandle();
 
                 var currentElement = targetElement;
                 while (true)
