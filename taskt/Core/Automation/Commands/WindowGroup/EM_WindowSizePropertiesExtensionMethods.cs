4087b5c45b6704a4dff0b5d451d7e24fb23e83b5
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-09-20 22:27:20 +0900

    change inner process

--- taskt/Core/Automation/Commands/WindowGroup/EM_WindowSizePropertiesExtensionMethods.cs
+++ taskt/Core/Automation/Commands/WindowGroup/EM_WindowSizePropertiesExtensionMethods.cs
@@ -9,17 +9,17 @@ namespace taskt.Core.Automation.Commands
     /// </summary>
     public static class EM_WindowSizePropertiesExtensionMethods
     {
-        /// <summary>
-        /// get window size
-        /// </summary>
-        /// <param name="whnd"></param>
-        /// <returns>(width, height)</returns>
-        public static (int, int) GetWindowSize(IntPtr whnd)
-        {
-            //var r = EM_WindowRECTPropertiesExtentionMethods.GetWindowRect(whnd);
-            //return (r.GetWidth(), r.GetHeight());
-            return WindowAPI.GetWindowSize(whnd);
-        }
+        ///// <summary>
+        ///// get window size
+        ///// </summary>
+        ///// <param name="whnd"></param>
+        ///// <returns>(width, height)</returns>
+        //public static (int, int) GetWindowSize(IntPtr whnd)
+        //{
+        //    //var r = EM_WindowRECTPropertiesExtentionMethods.GetWindowRect(whnd);
+        //    //return (r.GetWidth(), r.GetHeight());
+        //    return WindowAPI.GetWindowSize(whnd);
+        //}
 
         /// <summary>
         /// expand value or variable as Window Width
@@ -31,7 +31,8 @@ namespace taskt.Core.Automation.Commands
         public static int ExpandValueOrVariableAsWindowWidth(this IWindowSizeProperties command, IntPtr whnd, AutomationEngineInstance engine)
         {
             var w = command.v_Width;
-            (var width, var height) = GetWindowSize(whnd);
+            //(var width, var height) = GetWindowSize(whnd);
+            (var width, var height) = WindowAPI.GetWindowSize(whnd);
 
             if ((w == VariableNameControls.GetWrappedVariableName(SystemVariables.Window_CurrentSize.VariableName, engine)) ||
                  (w == VariableNameControls.GetWrappedVariableName(SystemVariables.Window_CurrentWidth.VariableName, engine)))
@@ -58,7 +59,8 @@ namespace taskt.Core.Automation.Commands
         public static int ExpandValueOrVariableAsWindowHeight(this IWindowSizeProperties command, IntPtr whnd, AutomationEngineInstance engine)
         {
             var w = command.v_Height;
-            (var width, var height) = GetWindowSize(whnd);
+            //(var width, var height) = GetWindowSize(whnd);
+            (var width, var height) = WindowAPI.GetWindowSize(whnd);
 
             if ((w == VariableNameControls.GetWrappedVariableName(SystemVariables.Window_CurrentSize.VariableName, engine)) ||
                  (w == VariableNameControls.GetWrappedVariableName(SystemVariables.Window_CurrentHeight.VariableName, engine)))
